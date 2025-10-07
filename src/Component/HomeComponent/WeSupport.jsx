427918c92932ae9f4d1b4d6c96a890e4242fd141
pankajqcs <pankajqcss@gmail.com>
2025-10-07 14:55:28 +0530

    responsive

--- src/Component/HomeComponent/WeSupport.jsx
+++ src/Component/HomeComponent/WeSupport.jsx
@@ -43,14 +43,14 @@ const WeSupport = ({ cards, heading, heading1 ,headingHeight}) => {
                             <SwiperSlide key={index} className="flex items-center justify-center ">
                                 <div className="bg-[#F5F7F9] rounded-lg p-4 h-[473px] flex flex-col sm:justify-between ">
                                     <div >
-                                        <h3 className={`font-semibold text-[20px] md:text-[25px] lg:text-[27px] 2xl:text-[32px] mb-2 font-unbounded  h-[69px] md:h-[${headingHeight}] w-full    `}>{item.heading}</h3>
+                                        <h3 className={`font-semibold   text-[20px] sm:text-[25px] md:text-[25px] lg:text-[27px] 2xl:text-[32px] mb-2 font-unbounded  h-[75px] md:h-[${headingHeight}] w-full sm:w-[68%] md:w-full   `}>{item.heading}</h3>
                                         {
-                                            item.desc && <p className={`text-gray-600 text-base h-[50px] lg:h-[70px] md:text-[18px] lg:text-[25px] font-medium font-unbounded `}>
+                                            item.desc && <p className={`text-gray-600 text-base xl:mt-5 h-[50px] lg:h-[70px] sm:text-[18px]  lg:text-[20px] font-medium font-unbounded `}>
                                                 {item.desc}
                                             </p> 
                                         }
-                                        <div className='flex flex-col items-start justify-between  h-60 mt-12 lg:mt-14 xl:mt-18'>
-                                            <p className='font-montserrat text-[13px] md:text-[15px] '>{item.para}</p>
+                                        <div className={`flex flex-col items-start justify-between    ${item.desc ? "mt-8 md:mt-8 lg:mt-15 xl:mt-10 md:h-64 lg:h-60  " : "mt-12 sm:mt-20 lg:mt-22 xl:mt-24 h-80 md:h-72 lg:h-64"}`}>
+                                            <p className='font-montserrat text-[15px] sm:text-[16px]  lg:text-[15px] '>{item.para}</p>
                                             <button className='flex gap-2 font-montserrat items-center justify-center'>{item.button} <span><MdKeyboardArrowRight className='text-[20px]' /></span></button>
                                         </div>
                                     </div>
